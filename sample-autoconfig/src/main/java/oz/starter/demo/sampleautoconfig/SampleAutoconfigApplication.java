7bcb587725602b310a24ef8d42460c44168721ac
Oleg Zhurakousky <ozhurakousky@pivotal.io>
2017-07-25 10:36:23 -0400

    Initial commit

---
+++ sample-autoconfig/src/main/java/oz/starter/demo/sampleautoconfig/SampleAutoconfigApplication.java
@@ -0,0 +1,12 @@
+package oz.starter.demo.sampleautoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAutoconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAutoconfigApplication.class, args);
+	}
+}
