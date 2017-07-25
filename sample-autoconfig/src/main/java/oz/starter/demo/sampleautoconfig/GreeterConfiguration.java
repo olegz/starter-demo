d3c89edaa8a832e2a0002c1244e6061f0d5642f1
Oleg Zhurakousky <ozhurakousky@pivotal.io>
2017-07-25 16:50:10 -0400

    STEP:2.2 Multiple beans of the same type - part-1 This step wires up two 'GreeterStrategy' beans, but by marking one as @Primary it still resolves successfully. By removing @Primary the following error mesage will be seen
    Field greeter in oz.myboot.app.samplebootapp.SampleBootAppApplication required a single bean, but 2 were found:
       - enGreeter: defined by method 'enGreeter' in class path resource [oz/starter/demo/sampleautoconfig/GreeterConfiguration.class]
       - frGreeter: defined by method 'frGreeter' in class path resource [oz/starter/demo/sampleautoconfig/GreeterConfiguration.class]

--- sample-autoconfig/src/main/java/oz/starter/demo/sampleautoconfig/GreeterConfiguration.java
+++ sample-autoconfig/src/main/java/oz/starter/demo/sampleautoconfig/GreeterConfiguration.java
@@ -1,18 +1,35 @@
 package oz.starter.demo.sampleautoconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import oz.greeter.GreeterStrategy;
 
 @Configuration
+@EnableConfigurationProperties
 public class GreeterConfiguration {
 
-	@Bean
+	@Autowired
+	private ApplicationContext context;
+
 	@ConditionalOnClass(name="oz.en.greeter.Greeter")
+	@Bean
 	public GreeterStrategy enGreeter() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
-		System.out.println("Creating English Greeter");
+		System.out.println("Creating English Greeter  " + context.getBean(GreeterStrategy.class));
 		return (GreeterStrategy)Class.forName("oz.en.greeter.Greeter").newInstance();
+	}
+
+	@Bean
+	@ConditionalOnClass(name="oz.fr.greeter.Greeter")
+	@Primary
+	public GreeterStrategy frGreeter() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		System.out.println("Creating French Greeter");
+		return (GreeterStrategy)Class.forName("oz.fr.greeter.Greeter").newInstance();
 	}
 }
